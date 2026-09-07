6beb1ff1baa1810564f45776cf51bb335e2aa603
namor222 <namor2@post.cz>
2026-09-07 04:05:19 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,17 @@
+const sklad = [
+    {
+        material: "Nerez",
+        druh: "Kulatina",
+        rozmer: "20 mm",
+        delka: "3000 mm",
+        atest: "11112",
+        pocet: 50
+    },
+    {
+        material: "Nerez",
+        druh: "Kulatina",
+        rozmer: "20 mm",
+        delka: "3000 mm",
+        atest: "11113",
+        pocet: 80
+    }
